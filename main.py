a0786c5f1cd6c1708cc027503879b62a54293d58
Contengent <103345406+Contengent@users.noreply.github.com>
2022-08-24 00:03:56 -0400

    what do I put here
    ok so i made way too many mistakes and I am just now getting back to working on this. I fixed a bucnh of the buggs, and also changed how keys are pressed by using the win32api since pynput is just as dumb as win32 when it comes to working in admin programs.
    I actually didn't check if pynput "special" keys work in admin programs, but ukno im lazy. I also don't think there would be any effiencet way to get them to work.
    regaurdless, it works more-or-less.
    no loops or any new features really, so :

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# SHOUT OUTS TO FUCKING NOBODY, WHY DOES WIN32API LETTER KEYS NOT WORK IN ADMIN PROCESSES?!?!?!??!?!
+# i need to rebuild this entire project in something like c++, idk i might shoot myself?
 # import modules
 import csv
+import time
+import win32api, win32con
 from pynput.mouse import Button, Controller as mouseController      # "as _Controller" is here because only the last module works when
 from pynput.keyboard import Key, Controller as keyboardController   # both of them are set to just "Controller".
 
@@ -7,6 +11,22 @@ from pynput.keyboard import Key, Controller as keyboardController   # both of th
 # apply functions to variables so stuff works and i guess also possible because of ^^^^^^^^^^
 mouse = mouseController()
 keyboard = keyboardController()
+
+
+# key dictoniantry bc it has to be streamliened
+DickToneAery = {
+    "left":  0x25,
+    "up":    0x26,
+    "right": 0x27,
+    "down":  0x28,
+
+    "ctrl":  0x11
+}
+
+def keyPressDown(keyValue):
+    win32api.keybd_event(keyValue, 0, 0, 0) # key-press event(key-hex, idk, press, idk)
+def keyReleaseUp(keyValue):
+    win32api.keybd_event(keyValue, 0, win32con.KEYEVENTF_KEYUP, 0)  # key-release event(key-hex, ?, release, ??)
 
 
 # function for the commands with 3 rows -- and "mouse-press" bc idk what to do with it.
@@ -27,20 +47,39 @@ def threeRowFunctions(row):
 def twoRowFunctions(row):
     match(row[0]): # checking the first row for the command name...
         case "key-down": # commnd name
-            print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
-            keyboard.press(Key.row[1]) # preforming the command!
-            print("works?")
+            if len(row[1]) > 1:
+                print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
+                keyPressDown(DickToneAery[row[1]])  # preforming the command!
+                print("don-oh")
+            else:
+                print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
+                keyboard.press(row[1]) # preforming the command
+                print("works?")
 
         # key-down & key-up don't seem to work, or atleast by themselves? They are untested.
         case "key-up":
-            print("Releasing key {}!".format(row[1]))
-            keyboard.release(Key.row[1])
+            if len(row[1]) > 1:
+                print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
+                keyReleaseUp(DickToneAery[row[1]])  # preforming the command!
+                print("don-oh")
+            else:
+                print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
+                keyboard.release(row[1]) # preforming the command
+                print("works?")
         
         case "key-press":
-            print("Pressing {} key down...".format(row[1]))
-            keyboard.down(Key.row[1])
-            print("Releasing key {}!".format(row[1]))
-            keyboard.release(Key.row[1])
+            if len(row[1]) > 1:
+                print("Pressing {} key down...".format(row[1])) # printing the command to be preformed
+                keyPressDown(DickToneAery[row[1]])  # preforming the command!
+                print("releasing key")
+                keyReleaseUp(DickToneAery[row[1]])
+                print("don")
+            else:
+                print("Pressing {} key down...".format(row[1]))
+                keyboard.press(row[1])
+                print("Releasing key {}!".format(row[1]))
+                keyboard.release(row[1])
+                print("don")
 
         case "type-out":
             print("Typing \"{}\"...".format(row[1]))
@@ -78,6 +117,7 @@ def twoRowFunctions(row):
         case "sleep":
             print("sleeping...")
             time.sleep(int(row[1]))
+            print("awake!")
 
 
 
